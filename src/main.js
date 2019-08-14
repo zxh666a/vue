6cd3224057fe7e1b096a014c7c4a7b685062957d
zxh <947966262@qq.com>
2019-08-14 15:57:05 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import router from './router.js'
 import app from './app.vue'
 import {Header} from 'mint-ui'
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 Vue.component(Header.name,Header)
 var vm = new Vue({
     el:'#app',
-    render:c => c(app)
+    render:c => c(app),
+    router
 })
--- src/router.js
+++ src/router.js
@@ -2,8 +2,8 @@ import VueRouter from 'vue-router'
 
 var router = new VueRouter({
     routes:[
-
-    ]
+    ],
+    linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类 router-link-active
 })
 
 export default router
