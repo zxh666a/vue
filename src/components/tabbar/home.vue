d03e50f93211653df31d6bfaec5e8cbd5c96445f
zxh <947966262@qq.com>
2019-08-15 21:32:22 +0800

    daka1

--- src/components/tabbar/home.vue
+++ src/components/tabbar/home.vue
@@ -11,10 +11,10 @@
     <!-- 六宫格 -->
     <ul class="mui-table-view mui-grid-view mui-grid-9">
       <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
+        <router-link to='/home/newlist'>
          <img src="../../image/新闻.png" alt="">
           <div class="mui-media-body">新闻资讯</div>
-        </a>
+        </router-link>
       </li>
       <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
         <a href="#">
--- src/router.js
+++ src/router.js
@@ -3,13 +3,15 @@ import home from './components/tabbar/home.vue'
 import member from './components/tabbar/member.vue'
 import search from './components/tabbar/search.vue'
 import shop from './components/tabbar/shop.vue'
+import newlist from './components/news/newlist.vue'
 var router = new VueRouter({
     routes:[
         {path:'/',redirect:'/home'},
         {path:'/home',component:home},
         {path:'/member',component:member},
         {path:'/search',component:search},
-        {path:'/shop',component:shop}
+        {path:'/shop',component:shop},
+        {path:'/home/newlist',component:newlist}
     ],
     linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类 router-link-active
 })
